313f0bad6534c39d230cb4e89e57fe314476b7a9
Patrick Cheng <pcheng86@gmail.com>
2023-07-27 08:40:16 -0700

    removed a lot of things that mentioned scrolling, added footer info, reversed legend polarity

--- js/main.js
+++ js/main.js
@@ -77,16 +77,33 @@ $("#screenshot-button").click(download_network);
 function download_network(e) {
   var svgEl = document.getElementById("sim-svg");
   svgEl.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-  var svgData = svgEl.outerHTML;
-  var preface = '<?xml version="1.0" standalone="no"?>\r\n';
-  var svgBlob = new Blob([preface, svgData], {type:"image/svg+xml;charset=utf-8"});
+
+  var serializer = new XMLSerializer();
+  var svgStr = serializer.serializeToString(svgEl);
+  var svgBlob = new Blob([svgStr], {type:"image/svg+xml;charset=utf-8"});
   var svgUrl = URL.createObjectURL(svgBlob);
-  var downloadLink = document.createElement("a");
-  downloadLink.href = svgUrl;
-  downloadLink.download = 'download.svg';
-  document.body.appendChild(downloadLink);
-  downloadLink.click();
-  document.body.removeChild(downloadLink);
+
+  var img = new Image();
+
+  img.onload = function() {
+    var canvas = document.createElement("canvas");
+    var ctx = canvas.getContext("2d");
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, 0, 0);
+
+    var pngDataUrl = canvas.toDataURL("image/png");
+    var downloadLink = document.createElement("a");
+    downloadLink.href = pngDataUrl;
+    downloadLink.download = 'network.png';
+    document.body.appendChild(downloadLink);
+    downloadLink.click();
+    document.body.removeChild(downloadLink);
+  };
+
+  img.src = svgUrl;
 }
 
 var interval = setInterval(run_Model, time_interval);
